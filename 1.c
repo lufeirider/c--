0960730b88c74eae8990304c5a6785e47df05575
lufeirider <3020207744@qq.com>
2016-07-10 22:17:05 +0800

    1.c
    创建新的文件

---
+++ 1.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	unsigned char a;
+	int i;
+	FILE *fp,*fp1;
+	fp=fopen("c:\\1.exe","rb+");
+	fp1=fopen("c:\\2.exe","wb+");
+
+	if(fp==NULL)
+	{
+		printf("open file error!\n");
+		return;
+	}
+
+	if(fp1==NULL)
+	{
+		printf("open file1 error!\n");
+		return;
+	}
+
+	while(!feof(fp))
+	{
+		ftell(fp);
+		int length=ftell(fp);
+		printf("%x",length);
+		if(length==2)
+		{
+			fread(&a,1,1,fp);
+			int b=0x99;
+			fwrite(&b, 1, 1, fp1);
+		}else{
+			fread(&a,1,1,fp);
+			int b=(int)a;
+			printf("%02x",a);
+			fwrite(&b, 1, 1, fp1);
+		}
+	}
+
+	printf("\n");
+	fclose(fp);
+	fclose(fp1);
+}
