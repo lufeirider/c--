c3f350cb0bb241b5ee0859817d8f34f3f3022c2b
lufeirider <3020207744@qq.com>
2016-08-18 20:28:10 +0800

    Create fwrite.c

---
+++ linux/fwrite.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+main()
+{
+	FILE *fp;
+	int i;
+	struct student{
+	char name[10];
+	int age;
+	}stu;
+
+	if((fp=fopen("test.txt","wb"))==NULL)
+	{
+		printf("不能打开文件");
+		exit(0);
+	}
+	
+	//stu.name = "test"; 这样是错误的不能将char[10]转成char*
+	//scanf("%s",stu.name);这样可以
+	//char name[10]="test";这样可以
+	strcpy(stu.name,"111");
+
+	fwrite(&stu.name,strlen(stu.name),1,fp);
+
+	fclose(fp);
+}
