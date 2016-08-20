f447f87c68f4ee08a340a9105a1a2087c4963f82
lufeirider <3020207744@qq.com>
2016-08-20 15:29:41 +0800

    Create msfpattern.py
    python msfpattern.py create 200
    python msfpattern.py offset 0x64413764

---
+++ shellcode/msfpattern.py
@@ -0,0 +1,32 @@
+import sys
+import struct
+
+def generate():
+    for x in xrange(0x41, 0x5b):
+        for y in xrange(0x61, 0x7b):
+            for z in xrange(0x30, 0x3a):
+                yield "%c%c%c" % (x, y, z)
+
+cmd = sys.argv[1]
+
+if cmd == 'create':
+    size = int(sys.argv[2])
+    s = ''
+    for x in generate():
+        s += x
+        if len(s) >= size:
+            print s[:size]
+            break
+    else:
+        raise Exception("size too large")
+elif cmd == 'offset':
+    value = int(sys.argv[2], 16)
+    chunk = struct.pack('<I', value)
+    s = ''
+    for x in generate():
+        s += x
+        if chunk in s:
+            print s.index(chunk)
+            break
+    else:
+        raise Exception("not found")
