01be284ce2345294848c246adec194bb1bcc455c
lufeirider <3020207744@qq.com>
2016-12-10 23:14:50 +0800

    Update FindWindow+ShowWindow.cpp

--- Study/FindWindow+ShowWindow.cpp
+++ Study/FindWindow+ShowWindow.cpp
@@ -3,6 +3,8 @@
 int main(int argc, char* argv[])
 {
 	HWND hwnd = ::FindWindow("notepad",NULL);
+	//HWND hwnd = ::FindWindow("ConsoleWindowClass",NULL);
+	//隐藏当前窗口	
 	if(hwnd)
 	{
 		  ::ShowWindow(hwnd,SW_HIDE);
