80d2a5e97be42e42316a8ceb4360c0e86e48afd9
lufeirider <3020207744@qq.com>
2016-12-19 15:50:11 +0800

    Create UdpServer.cpp

---
+++ Study/UdpServer.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"  
+#include <stdio.h>  
+#include <winsock2.h>  
+  
+#pragma comment(lib, "ws2_32.lib")   
+  
+int main(int argc, char* argv[])  
+{  
+    WSADATA wsaData;  
+    WORD sockVersion = MAKEWORD(2,2);  
+    if(WSAStartup(sockVersion, &wsaData) != 0)  
+    {  
+        return 0;  
+    }  
+  
+    SOCKET serSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);   
+    if(serSocket == INVALID_SOCKET)  
+    {  
+        printf("socket error !");  
+        return 0;  
+    }  
+  
+    sockaddr_in serAddr;  
+    serAddr.sin_family = AF_INET;  
+    serAddr.sin_port = htons(8888);  
+    serAddr.sin_addr.S_un.S_addr = INADDR_ANY;  
+    if(bind(serSocket, (sockaddr *)&serAddr, sizeof(serAddr)) == SOCKET_ERROR)  
+    {  
+        printf("bind error !");  
+        closesocket(serSocket);  
+        return 0;  
+    }  
+      
+    sockaddr_in remoteAddr;  
+    int nAddrLen = sizeof(remoteAddr);   
+    while (true)  
+    {  
+        char recvData[255];    
+        int ret = recvfrom(serSocket, recvData, 255, 0, (sockaddr *)&remoteAddr, &nAddrLen);  
+        if (ret > 0)  
+        {  
+            recvData[ret] = 0x00;  
+            printf("接受到一个连接：%s \r\n", inet_ntoa(remoteAddr.sin_addr));  
+            printf(recvData);              
+        }  
+  
+        char * sendData = "一个来自服务端的UDP数据包\n";  
+        sendto(serSocket, sendData, strlen(sendData), 0, (sockaddr *)&remoteAddr, nAddrLen);      
+  
+    }  
+    closesocket(serSocket);   
+    WSACleanup();  
+    return 0;  
+}  
